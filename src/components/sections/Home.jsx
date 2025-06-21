f7161ffc2965f5b31b2b3371d05c97528fc19fa8
rakibhassannayem <rakibhassannayem@gmail.com>
2025-06-21 21:04:36 +0600

    update home

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         </h1>
 
         <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-          I'm a full—stack developer who loves crafting clean, scalable web
+          I'm a frontend developer who loves crafting clean, scalable web
           applications. My goal is to build solutions that offer both
           exceptional performance and a delightful user experience.
         </p>
